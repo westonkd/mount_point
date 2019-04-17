9ad3d9320a94096568e02d3b4e55fcc04dabf922
Weston Dransfield <wdrans@gmail.com>
2019-04-16 18:50:24 -0600

    update helper

--- lib/mount_point/generators/component_mount_point.rb
+++ lib/mount_point/generators/component_mount_point.rb
@@ -1,7 +1,12 @@
 module MountPoint
   module Generators
-    def banana(text)
-      content_tag :pre, text
+    def mount_point(component_name, props: {}, as: :div)
+      content_tag(
+        as,
+        '',
+        'id' => "#{component_name}_mount_point",
+        'data-props' => JSON.stringify(props)
+      )
     end
   end
 end
