ca86ace6bc85fbe2a286bb9b9bdff222d8b35f51
Weston Dransfield <wdrans@gmail.com>
2019-04-16 18:59:48 -0600

    convert props to json

--- lib/mount_point/generators/component_mount_point.rb
+++ lib/mount_point/generators/component_mount_point.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module MountPoint
   module Generators
     def mount_point(component_name, props: {}, as: :div)
@@ -5,7 +7,7 @@ module MountPoint
         as,
         '',
         'id' => "#{component_name}_mount_point",
-        'data-props' => JSON.stringify(props)
+        'data-props' => props.to_json
       )
     end
   end
